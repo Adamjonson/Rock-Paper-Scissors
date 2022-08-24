53f22a7e0d86cd325965758970fa76e652df6404
Adhamjon <adkhamjon.abd@gmail.com>
2022-08-24 17:13:00 +0500

    Add a new function

---
+++ logic.js
@@ -0,0 +1,8 @@
+// Randomly returns ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice() {
+    // the outcomes
+    const outcomes = ["Rock", "Paper", "Scissors"]
+    let randomNumber = Math.random() * 3;
+    let result = outcomes[randomNumber];
+    return result;
+}
